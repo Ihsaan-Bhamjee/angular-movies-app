c6f4c62a59e07900715137761f7f4112eaae42fe
Bhamjee <bhamjeei@legal.regn.net>
2024-09-25 14:21:24 +0200

    add form to login page

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,13 +1,30 @@
 import { Component } from '@angular/core';
 import { NgOptimizedImage } from '@angular/common'
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [NgOptimizedImage],
+  imports: [NgOptimizedImage, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+    loginForm = new FormGroup({
+        username: new FormControl(''),
+        password: new FormControl(''),
+    })
+    errorMsg = '' ;
 
+    login() {
+        if (this.loginForm.value.username?.trim().length === 0) {
+            this.errorMsg = "Username is required";
+        }
+        else if (this.loginForm.value.password?.trim().length === 0) {
+            this.errorMsg = "Password is required";
+        }
+        else {
+            this.errorMsg = '';
+        }
+    }
 }
